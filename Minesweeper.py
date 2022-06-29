dce468d9f59f17f8c6cca023e35e1ce392befdd4
AlexanderD42 <alexgd2016@gamil.com>
2022-06-29 17:15:39 -0400

    initial

---
+++ Minesweeper.py
@@ -0,0 +1,63 @@
+import pygame
+import random
+from Settings import board
+import time
+pygame.init()
+
+WIDTH, HEIGHT = 400,400
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Minesweeper Alex Addition')
+screen.fill((0, 0, 0))
+board1 = board(10, 12, 10, screen)
+board1.draw()
+Loser = ['Good Job, You Lost!', 'You do know that you dont click the bombs...right...?', 'Loser', 'Defeat', 'You have died...']
+
+
+
+
+
+while True:
+  
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+    
+
+    if event.type == pygame.MOUSEBUTTONDOWN:
+      if event.button == 1:
+        pos = pygame.mouse.get_pos()
+        for t in range(len(board1.board)):
+          for g in range(len(board1.board[t])):
+            if board1.board[t][g].rect.collidepoint(pos) and board1.board[t][g].flag == False and board1.board[t][g].hide == False:
+              print(t, g)
+              if board1.board[t][g].bomb == True:
+                pygame.display.flip()
+                time.sleep(1)
+                pygame.quit()
+                print(Loser[random.randint(0, 4)])
+              else:
+                board1.pick(t, g)
+                board1.draw()
+                #print(pos)
+              
+               
+                
+                
+          
+      if event.button == 3:
+        pos = pygame.mouse.get_pos()
+        for t in board1.board:
+          for g in t:
+            if g.rect.collidepoint(pos):
+              g.flag ^= True
+              print(g.flag)
+              board1.draw()
+              #print(pos)
+
+    
+              
+    
+
+
+  pygame.display.flip()
---
+++ Settings.py
@@ -0,0 +1,216 @@
+import pygame
+import random
+import time
+
+class tile: 
+  bombs = 0
+  def __init__(self, y, x):
+    self.rect = pygame.Rect(x*board.screen.get_width()/board.column, y*board.screen.get_height()/board.row, board.screen.get_width()/board.column, board.screen.get_height()/board.row)
+    self.bomb = False
+    self.number = 0
+    self.flag = False
+    self.hide = False
+    self.tile = pygame.image.load('CoveredTile.png')
+    self.tile = pygame.transform.scale(self.tile, (self.rect.right-self.rect.left, self.rect.bottom-self.rect.top))
+    self.tileRect = self.tile.get_rect()
+    self.tileRect.topleft = self.rect.topleft
+    self.bombIMG = pygame.image.load('Bomb.png')
+    self.bombIMG = pygame.transform.scale(self.bombIMG, (self.rect.right-self.rect.left, self.rect.bottom-self.rect.top))
+    self.bombRect = self.bombIMG.get_rect()
+    self.bombRect.topleft = self.rect.topleft
+    self.flagIMG = pygame.image.load('Flag.png')
+    self.flagIMG = pygame.transform.scale(self.flagIMG, (self.rect.right-self.rect.left, self.rect.bottom-self.rect.top))
+    self.flagRect = self.flagIMG.get_rect()
+    self.flagRect.topleft = self.rect.topleft
+    self.font = pygame.font.SysFont('timesnewroman', 30)
+    
+
+  def draw(self):
+    if self.hide == True:
+      pygame.draw.rect(board.screen, (255, 255, 255), self.rect)
+    if self.bomb:
+      board.screen.blit(self.bombIMG,  self.bombRect)
+
+    else:
+      if self.number > 0:
+        textRect = self.rect
+        textRect.x += 15
+        board.screen.blit(self.font.render (str(self.number), True, (0, 0, 0)), textRect)
+        self.rect.x -= 15
+        
+    if self.hide == False:
+
+      board.screen.blit(self.tile,  self.tileRect)
+
+    if self.flag:
+      board.screen.blit(self.flagIMG,  self.flagRect)
+
+
+    
+
+
+class board:
+  
+  def __init__(self, column, row, bombs, screen):
+    board.column = column
+    board.row = row
+    self.bombs = bombs
+    board.screen = screen
+    self.Count = board.row*board.column-self.bombs
+    
+ 
+          
+     
+  
+    self.board = []
+    for i in range(row):
+      self.board.append([])
+      for j in range(column):
+        
+        self.board[i].append(tile(i, j))
+        
+    bombsPlaced = bombs
+    while bombsPlaced > 0:
+      #print(bombsPlaced)
+      # Adds all bombs and updates the numbers
+      for h in range(row):
+        for k in range(column):
+          if bombsPlaced > 0:
+
+            if random.randint(0, 35) == 1 and self.board[h][k].bomb == False:
+              
+              self.board[h][k].bomb = True
+              # Top Left Corner
+              if h == 0 and k == 0:
+                self.board[h][k+1].number += 1
+                self.board[h+1][k].number += 1
+                self.board[h+1][k+1].number += 1
+
+              # Top Right Corner
+              elif h == 0 and k == column-1:
+
+                self.board[h][k-1].number += 1
+                self.board[h+1][k-1].number += 1
+                self.board[h+1][k].number += 1
+
+              # Bottom Left Corner
+              elif h == row-1 and k == 0:
+                self.board[h-1][k].number += 1
+                self.board[h][k+1].number += 1
+                self.board[h-1][k+1].number += 1
+
+
+              # Bottom right corner
+              elif h == row-1 and k == column-1:
+                self.board[h-1][k].number += 1
+                self.board[h][k-1].number += 1
+                self.board[h-1][k-1].number += 1
+
+              # Checking sides
+              else:
+                # Top row
+                if h == 0:
+                  self.board[h+1][k].number += 1
+                  self.board[h][k-1].number += 1
+                  self.board[h][k+1].number += 1
+                  self.board[h+1][k+1].number += 1
+                  self.board[h+1][k-1].number += 1
+
+                # Bottom row
+                elif h == row-1:
+                  self.board[h-1][k].number += 1
+                  self.board[h][k-1].number += 1
+                  self.board[h][k+1].number += 1
+                  self.board[h-1][k-1].number += 1
+                  self.board[h-1][k+1].number += 1
+
+                # Left column
+                elif k == 0:
+                  self.board[h+1][k].number += 1
+                  self.board[h-1][k].number += 1
+                  self.board[h][k+1].number += 1
+                  self.board[h-1][k+1].number += 1
+                  self.board[h+1][k+1].number += 1
+
+                # Right column
+                elif k == column-1:
+                  self.board[h-1][k].number += 1
+                  self.board[h+1][k].number += 1
+                  self.board[h][k-1].number += 1
+                  self.board[h-1][k-1].number += 1
+                  self.board[h+1][k-1].number += 1
+
+                # not on the sides of the board
+                else:
+                  self.board[h-1][k].number += 1
+                  self.board[h+1][k].number += 1
+                  self.board[h][k-1].number += 1
+                  self.board[h][k+1].number += 1
+                  self.board[h-1][k-1].number += 1
+                  self.board[h-1][k+1].number += 1
+                  self.board[h+1][k+1].number += 1
+                  self.board[h+1][k-1].number += 1
+              tile.bombs += 1
+              bombsPlaced -= 1
+              
+    
+    print(tile.bombs)
+  #def bombs(self, row, column, bombs):
+       # self.board = []
+    #if row >= 0 and column >=0 and row < self.row and column < self.column:
+      
+
+  def draw(self):
+    for h in self.board:
+        for k in h:
+          k.draw()
+
+  def pick(self, row, column):
+    #print(row, column)
+    
+    if row >= 0 and column >=0 and row < self.row and column < self.column:
+      
+      value = self.board[row][column].number
+      
+    else:
+
+      return
+
+    
+
+
+    if value > 0 and not self.board[row][column].hide:
+      #print('hello')
+      self.board[row][column].hide = True
+      self.Count -=1
+      return 1
+    
+    elif value == 0 and not self.board[row][column].hide:
+      self.board[row][column].hide = True
+      self.draw()
+      pygame.display.update()
+     
+      print(row,column)
+      self.Count -=1
+      self.pick(row-1, column-1)
+      self.pick(row-1, column)
+      self.pick(row+1, column+1)
+      self.pick(row, column-1)
+      self.pick(row, column+1)
+      self.pick(row+1, column-1)
+      self.pick(row+1, column)
+      self.pick(row+1, column+1)
+
+    
+   
+    if self.Count == 0:
+      print('you have won')
+      pygame.quit()
+  
+
+
+
+
+
+      
+
